f80f787d5dcf2dbd2fee6cd2988fbe7cf502d449
Jaejejye <seratona6484@gmail.com>
2022-03-23 18:10:42 +0900

    unit 7_예제

---
+++ Day 01/simple.py
@@ -0,0 +1,8 @@
+a, b, c = map(int, input('숫자 3개를 입력하세요.'))
+
+print("3개의 숫자 : ", a, b, c)
+print("합계 : ", a, b, c)
+
+# cmd -> Workspace\Python\Day 01>conda active base
+# (base) ~>python sample.py   ---> sample.py 파일을 cmd를 통해 열기
+# -> 해당 방법으로 cmd에서도 실행 가능
